45010c11706362f203327d1c374b2151a613807e
zarame1234 <zarame1234@users.noreply.github.com>
2025-06-26 22:34:45 +0900

    コンストラクタ

---
+++ practice9/Thief.java
@@ -0,0 +1,15 @@
+public class Thief {
+  String name;
+  int hp;
+  int mp;
+
+  public Thief(String name, int hp, int mp) {
+    this.name = name;
+    this.hp = hp;
+    this.mp = mp;
+  }
+
+  public Thief(String name, int hp) {
+    this(name, hp, 5);
+  }
+}
